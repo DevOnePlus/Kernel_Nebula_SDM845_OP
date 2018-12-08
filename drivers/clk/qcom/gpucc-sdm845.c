7857d6ed0049b89ff9e01ebeb74110c270accdc0
Eliminater74 <eliminater74@gmail.com>
2018-12-07 22:45:13 -0500

    [clk] [GPU] Overclock GPU 833mhz:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -282,8 +282,8 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(596000000, P_CRC_DIV,  1, 0, 0),
 	F(675000000, P_CRC_DIV,  1, 0, 0),
 	F(710000000, P_CRC_DIV,  1, 0, 0),
-	F(750000000, P_CRC_DIV,  1, 0, 0),
-	F(816000000, P_CRC_DIV,  1, 0, 0),
+	F(760000000, P_CRC_DIV,  1, 0, 0),
+	F(833000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
 
@@ -638,9 +638,9 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 		520000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL_L1] =
 		675000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 750000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 760000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
-		816000000;
+		833000000;
 }
 
 static void gpu_cc_gfx_sdm845_fixup_sdm670(void)
