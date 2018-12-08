ea7f897c0613e481715b42ac73f4edafcdcda324
Eliminater74 <eliminater74@gmail.com>
2018-12-08 15:16:48 -0500

    [clk] [GPU] Tune Voltage Settings:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -283,7 +283,6 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(675000000, P_CRC_DIV,  1, 0, 0),
 	F(710000000, P_CRC_DIV,  1, 0, 0),
 	F(760000000, P_CRC_DIV,  1, 0, 0),
-	F(833000000, P_CRC_DIV,  1, 0, 0),
 	F(855000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
@@ -321,9 +320,9 @@ static struct clk_rcg2 gpu_cc_gx_gfx3d_clk_src = {
 			LOW, 280000000,
 			LOW_L1, 338000000,
 			NOMINAL, 425000000,
-			NOMINAL_L1, 487000000,
-			HIGH, 548000000,
-			HIGH_L1, 600000000),
+			NOMINAL_L1, 490000000,
+			HIGH, 560000000,
+			HIGH_L1, 625000000),
 	},
 };
 
